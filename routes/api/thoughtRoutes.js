c77a6a5920e8ea76f41923f87843d91aa2afdc59
Mark Lewis <mlewis89@gmail.com>
2024-04-22 16:04:07 +1000

    user routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {getUsers, 
+    getOneUser, 
+    createUser, 
+    updateUser, 
+    deleteUser, 
+    clearUserThoughts} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:courseId
+router
+  .route('/:courseId')
+  .get(getOneUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
