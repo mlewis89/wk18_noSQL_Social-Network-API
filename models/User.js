29060c42cd7b1420a317c7e4e1bd0b05d20c6249
Mark Lewis <mlewis89@gmail.com>
2024-04-22 16:57:01 +1000

    user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //validate email*************************************************************************************
+    },
+    thoughts : [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'thought',
+        },
+      ],
+      friends : [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'user',
+        },
+      ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const User = model('user', UserSchema);
+
+module.exports = assignmentSchema;
